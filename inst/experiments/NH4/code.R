4432fa50a66381f294f39fb851b5bfe66ff146db
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2019-05-05 14:42:10 -0700

    more

--- inst/experiments/NH4/code.R
+++ inst/experiments/NH4/code.R
@@ -10,17 +10,19 @@ doExpt <- function()
    y <- x[,11]  
    x <- x[,-11]
    ccrows <- which(complete.cases(x))
+   allnarows <- which(apply(x,1,function(rw) all(is.na(rw))))
+   
    xcc <- x[ccrows,]
    ycc <- y[ccrows]
    lmout <- lm(y ~ x)
 
-   # predict on the incomplete (wrt X) cases
-   xncc <- x[-ccrows,]
-   checkallna <- apply(xncc,1,function(rw) all(is.na(rw)))
-   ccrows <- setdiff(ccrows,checkallna)
-   xncc <- x[-ccrows,]
-   yncc <- y[-ccrows]
+   # predict on the incomplete (wrt X) cases; but must skip those that
+   # are totally incomplete
+   allnarows <- which(apply(x,1,function(rw) all(is.na(rw))))
+   use <- setdiff(1:nrow(x),union(ccrows,allnarows))
    browser()
+   xncc <- x[use,]
+   yncc <- y[use]
    towerout <- toweranNA(xcc,lmout$fitted.values,5,xncc,scaleX=FALSE)
 
 }
