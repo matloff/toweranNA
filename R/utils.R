12f2d94a0d0061335354f73a90617d15952605e2
matloff <matloff@cs.ucdavis.edu>
2019-05-08 21:12:12 -0700

    added convertToDummies()

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,7 @@
 
+
+########################  countnas()  ###############################
+
 # see how many NAs in each column of df; mvchar is the character
 # indicating missingness, e.g. '?', if not coded NA
 
@@ -8,6 +11,8 @@ countnas <- function(df,mvchar=NULL)  {
    else
       sapply(df,function(cl) sum(cl == mvchar))
 }
+
+########################  checkConstCols()  ###############################
 
 # report whether any columns in data frame are constant, including the
 # case of constant value plus some NAs
@@ -24,5 +29,21 @@ checkConstCol <- function(cl)
    if (nuniq == 1) return(TRUE)
    if (nuniq == 2 && sum(is.na(cl)) > 0) return(TRUE)
    return(FALSE)
+}
+
+########################  convertToDumms()  ###############################
+
+# takes the factor xf with k levels excl. 'na', and converts it to a
+# data frame of k columns, one for each non-'na' level, with labels of
+# the form u.v, u being the original col name and v being the level
+
+convertToDumms <- function(xfr,lvls,xfrname) 
+{
+   require(dummies)
+   tmp <- dummy(xfr,sep='.')
+   tmp[['xfr.na']] <- NULL
+   names(tmp) <- gsub('xfr.','',names(tmp))
+   lvls1 <- lvls[lvls != 'na']
+   tmp[,lvls1]
 }
 
