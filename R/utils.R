9b09a2ae1c4236c86f3fffeba36f46305f68f7fd
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2019-05-10 15:14:08 -0700

    finally right

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,14 @@
 
 ########################  countnas()  ###############################
 
-# see how many NAs in each column of df; mvchar is the character
+# see how many NAs in each column of dfr; mvchar is the character
 # indicating missingness, e.g. '?', if not coded NA
 
-countnas <- function(df,mvchar=NULL)  {
+countnas <- function(dfr,mvchar=NULL)  {
    if (is.null(mvchar)) 
-      sapply(df,function(cl) sum(is.na(cl)))
+      sapply(dfr,function(cl) sum(is.na(cl)))
    else
-      sapply(df,function(cl) sum(cl == mvchar))
+      sapply(dfr,function(cl) sum(cl == mvchar))
 }
 
 ########################  checkConstCols()  ###############################
@@ -17,9 +17,9 @@ countnas <- function(df,mvchar=NULL)  {
 # report whether any columns in data frame are constant, including the
 # case of constant value plus some NAs
 
-checkConstCols <- function(df) 
+checkConstCols <- function(dfr) 
 {
-   sapply(df,checkConstCol)
+   sapply(dfr,checkConstCol)
 }
 
 # 1-column version
@@ -31,19 +31,54 @@ checkConstCol <- function(cl)
    return(FALSE)
 }
 
-########################  convertToDumms()  ###############################
+########################  factorToDumms()  ###############################
 
-# takes the factor xf with k levels excl. 'na', and converts it to a
-# data frame of k columns, one for each non-'na' level, with labels of
-# the form u.v, u being the original col name and v being the level
+# converts the factor x to a matrix, via dummy(), but properly handling
+# NAs; nm is an optional col name
 
-convertToDumms <- function(xfr,lvls,xfrname) 
+factorToDumms <- function(x,nm=NULL)
 {
    require(dummies)
-   tmp <- dummy(xfr,sep='.')
-   tmp[['xfr.na']] <- NULL
-   names(tmp) <- gsub('xfr.','',names(tmp))
-   lvls1 <- lvls[lvls != 'na']
-   tmp[,lvls1]
+   tmp <- dummy(x,sep='.')
+   if (any(is.na(x))) {
+      # need to make NA all dummies corresponding to an NA
+      # get name of x
+      xname <- as.character(sys.call(1))[2]
+      nm <- xname
+      # get name of the NA column
+      nacolnm <- paste0(xname,'.NA')
+      # which col is it?
+      nacol <- which(colnames(tmp) == nacolnm)
+      # jettison it
+      tmp <- tmp[,-nacol]
+      # where are the NAs?
+      nas <- which(is.na(x))
+      # properly make those rows NAs
+      tmp[nas,] <- NA
+   }
+   colnames(tmp) <- paste0(nm,'_',levels(x))
+   tmp
 }
 
+######################## dfrFactorsToDumms()  ###############################
+
+# for each factor in dfr, replace by dummies; if omitLast, then the last
+# column of each set of dummies will be omitted; calls factorToDumms()
+
+dfrFactorsToDumms <- function(dfr,omitLast=FALSE) 
+{
+   if (!is.data.frame(dfr)) stop('dfr must be a data frame (or data.table)')
+   browser()
+   toNull <- NULL
+   for (i in 1:ncol(dfr)) {
+      coli <- dfr[,i]
+      if (is.factor(coli)) {
+         dumms <- factorToDumms(coli,names(dfr)[i])
+         if (omitLast) dumms <- dumms[,-ncol(dumms)]
+         toNull <- c(toNull,i)
+         dfr <- cbind(dfr,dumms)
+      }
+   }
+   dfr[,toNull] <- NULL
+   dfr
+}
