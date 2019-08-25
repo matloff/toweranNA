6754d678ec5c2a057817889e7a2523da3e47ff6c
Norman Matloff <matloff@cs.ucdavis.edu>
2019-08-25 16:11:58 -0700

    new option in countnas

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,17 @@
 
 ########################  countnas()  ###############################
 
-# see how many NAs in each column of dfr; mvchar is the character
-# indicating missingness, e.g. '?', if not coded NA
+# see how many NAs in each column of dfr; counts are reported unles prop
+# is TRUE, in which case proportions of NAs are shown
 
-countnas <- function(dfr,mvchar=NULL)  {
-   if (is.null(mvchar)) 
-      sapply(dfr,function(cl) sum(is.na(cl)))
-   else
-      sapply(dfr,function(cl) sum(cl == mvchar))
+countnas <- function(dfr,prop=FALSE)  {
+   countFtn <- function(cl) 
+   {
+      tmp <- sum(is.na(cl))
+      if (prop) tmp <- tmp / length(cl)
+      tmp
+   }
+   sapply(dfr,countFtn)
 }
 
 ########################  checkConstCols()  ###############################
@@ -31,53 +34,3 @@ checkConstCol <- function(cl)
    return(FALSE)
 }
 
-########################  factorToDumms()  ###############################
-
-# converts the factor x to a matrix, via dummy(), but properly handling
-# NAs; nm is an optional col name
-
-factorToDumms <- function(x,nm=NULL)
-{
-   require(dummies)
-   tmp <- dummy(x,sep='.')
-   if (any(is.na(x))) {
-      # need to make NA all dummies corresponding to an NA
-      # get name of x
-      xname <- as.character(sys.call(1))[2]
-      if (is.null(nm)) nm <- xname
-      # get name of the NA column
-      nacolnm <- paste0(xname,'.NA')
-      # which col is it?
-      nacol <- which(colnames(tmp) == nacolnm)
-      # jettison it
-      tmp <- tmp[,-nacol]
-      # where are the NAs?
-      nas <- which(is.na(x))
-      # properly make those rows NAs
-      tmp[nas,] <- NA
-   }
-   colnames(tmp) <- paste0(nm,'_',levels(x))
-   tmp
-}
-
-######################## dfrFactorsToDumms()  ###############################
-
-# for each factor in dfr, replace by dummies; if omitLast, then the last
-# column of each set of dummies will be omitted; calls factorToDumms()
-
-dfrFactorsToDumms <- function(dfr,omitLast=FALSE) 
-{
-   if (!is.data.frame(dfr)) stop('dfr must be a data frame (or data.table)')
-   toNull <- NULL
-   for (i in 1:ncol(dfr)) {
-      coli <- dfr[,i]
-      if (is.factor(coli)) {
-         dumms <- factorToDumms(coli,names(dfr)[i])
-         if (omitLast) dumms <- dumms[,-ncol(dumms)]
-         toNull <- c(toNull,i)
-         dfr <- cbind(dfr,dumms)
-      }
-   }
-   dfr[,toNull] <- NULL
-   dfr
-}
